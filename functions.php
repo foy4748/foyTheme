7ba592af76defbec50f06162b573f10ed29cc42f
f-rahman23 <f.rahman@iideainformatics.com>
2023-06-17 13:17:02 +0600

    Added site title change control in WPD

--- functions.php
+++ functions.php
@@ -3,15 +3,31 @@
 // Theme Title
 add_theme_support('title-tag');
 
-function enqueueing_styles_and_scripts(){
+/*
+ * Registering and Enqueueing Stylesheets and Scripts
+ *
+ */
+function enqueueing_styles_and_scripts(){ //Connection1
 	wp_enqueue_style( 'style.css' );
 	wp_register_style('foy4748_custom_style', get_template_directory_uri().'/css/custom.css', array(), '1.0', 'all');
 	wp_enqueue_style('foy4748_custom_style');
+
+	wp_enqueue_script('jquery');
 }
 
-add_action('wp_enqueue_scripts', 'enqueueing_styles_and_scripts');
+add_action('wp_enqueue_scripts', 'enqueueing_styles_and_scripts'); //Connection1 
 
-function customizing_wpd($wp_customize){
+
+/*
+ * Wordpress Dashboard (WPD)
+ * Customizer
+ *
+ */
+function customizing_wpd($wp_customize){ //Connection2
+	/*
+	 * Logo Customizer
+	 *
+	 */
 	$wp_customize-> add_section('header_area', array(
 		'title'=>'Choose Logo'
 	));
@@ -26,7 +42,28 @@ function customizing_wpd($wp_customize){
 		'settings'=>'brand_logo',
 		'section'=>'header_area'
 	)));
+	/* --------------------
+	 */
 
+	/*
+	 * Title Customizer
+	 *
+	 */
+	$wp_customize-> add_section('site_title_section', array(
+		'title'=>"Change Site title"
+	));
+
+	$wp_customize-> add_setting('site_title_setting', array(
+		'default'=>'My First Web Site Ever',
+		'sanitie_callback'=>'sanitize_text_field'
+	));
+
+	$wp_customize-> add_control('site_title_control', array(
+		'label'=>"Change Site Title as you like",
+		'section'=>'site_title_section',
+		'settings'=>'site_title_setting',
+		'type'=>'text'
+	));
 }
 
-add_action('customize_register', 'customizing_wpd');
+add_action('customize_register', 'customizing_wpd'); //Connection2
--- index.php
+++ index.php
@@ -11,6 +11,7 @@
 	<?php wp_head(); ?>
 </head>
 <body>
+<h1><?php echo get_theme_mod('site_title_setting'); ?></h1>
 <img src="<?php echo get_theme_mod('brand_logo'); ?>" alt="brand logo" style="width:150px;">
 <?php wp_footer(); ?>
 </body>
